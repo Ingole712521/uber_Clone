d0f3eb6a49b4f8e9da874628455c1a872babb044
Ingole712521 <nehalingole2001@gmail.com>
2024-11-27 20:37:39 +0530

    Backend file

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const http = require("http");
+const app = require("./App");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`App is listen on PORT number ${port}`);
+});
