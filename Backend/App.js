b04f9e59270faf69e1d482d6dfffdd14dd067cc8
Ingole712521 <nehalingole2001@gmail.com>
2024-11-28 12:04:51 +0530

    change in file

--- Backend/App.js
+++ Backend/App.js
@@ -9,4 +9,5 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,10 @@ const app = require("./App");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-
 server.listen(port, () => {
   console.log(`App is listen on PORT number ${port}`);
+});
+
+http.createServer(function (res, req) {
+  res.writeHead("2");
 });
